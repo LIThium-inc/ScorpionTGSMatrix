ec25dacaf4311def1651c1c80cf873a2c5e688bf
LITHIUMinc <nikolaypetrenkin@gmail.com>
2024-07-11 14:52:47 +0300

    Initial commit

---
+++ scorpion_tgs_matrix/matrix.py
@@ -0,0 +1,80 @@
+data = open('test.txt', 'r')
+
+
+def remove_zero(array):
+    while 0.0 in array:
+        array.remove(0.0)
+
+
+# noinspection t
+def create_energy_matrix(matrix, array, pixels_per_event=1, min_filter=0.0):
+    """Create a matrix of energy distribution.
+
+        @param: matrix A matrix to save parameters
+        @param: array A string with energy data
+        @param: pixels_per_event The count of activated pixels per one event (default 1)
+        @param: min_filter Allows to save data only with energy above filter parameter (default 0.0)
+
+        """
+
+    zeroless_x, array_x = array[:8], array[:8]
+    zeroless_y, array_y = array[8:], array[8:]
+    remove_zero(zeroless_x)
+    remove_zero(zeroless_y)
+    if pixels_per_event > 1:
+        if ((len(zeroless_x) == pixels_per_event or len(zeroless_y) == pixels_per_event)
+                and zeroless_x[0] >= min_filter and zeroless_y[0] >= min_filter):
+            if len(zeroless_x) == pixels_per_event and len(zeroless_y) == 1:
+                for j in range(len(zeroless_x)):
+                    x = array_x.index(zeroless_x[j])
+                    y = array_y.index(zeroless_y[0])
+                    matrix[x, y] = matrix[x, y] + zeroless_x[j]
+            if len(zeroless_y) == pixels_per_event and len(zeroless_x) == 1:
+                for j in range(len(zeroless_y)):
+                    x = array_x.index(zeroless_x[0])
+                    y = array_y.index(zeroless_y[j])
+                    print(f"X: {x}, Y: {y}")
+                    matrix[x, y] = matrix[x, y] + zeroless_y[j]
+    elif pixels_per_event == 1:
+        if len(zeroless_x) == len(zeroless_y) == 1 and zeroless_x[0] >= min_filter and zeroless_y[0] >= min_filter:
+            print(zeroless_x[0], zeroless_y[0])
+            x = array_x.index(zeroless_x[0])
+            y = array_y.index(zeroless_y[0])
+            print(f"X: {x}, Y: {y}")
+            matrix[x, y] = matrix[x, y] + zeroless_x[0]
+
+
+# noinspection t
+def create_events_count_matrix(matrix, array, pixels_per_event=1, min_filter=0.0):
+    """Create a matrix of energy distribution.
+
+        @param: matrix A matrix to save parameters
+        @param: array A string with energy data
+        @param: pixels_per_event The count of activated pixels per one event (default 1)
+        @param: min_filter Allows to save data only with energy above filter parameter (default 0.0)
+
+        """
+    zeroless_x, array_x = array[:8], array[:8]
+    zeroless_y, array_y = array[8:], array[8:]
+    remove_zero(zeroless_x)
+    remove_zero(zeroless_y)
+    if pixels_per_event > 1:
+        if ((len(zeroless_x) == pixels_per_event or len(zeroless_y) == pixels_per_event)
+                and zeroless_x[0] >= min_filter and zeroless_y[0] >= min_filter):
+            if len(zeroless_x) == pixels_per_event and len(zeroless_y) == 1:
+                for j in range(len(zeroless_x)):
+                    x = array_x.index(zeroless_x[j])
+                    y = array_y.index(zeroless_y[0])
+                    matrix[x, y] = matrix[x, y] + 1
+            if len(zeroless_y) == pixels_per_event and len(zeroless_x) == 1:
+                for j in range(len(zeroless_y)):
+                    x = array_x.index(zeroless_x[0])
+                    y = array_y.index(zeroless_y[j])
+                    matrix[x, y] = matrix[x, y] + 1
+    elif pixels_per_event == 1:
+        if len(zeroless_x) == len(zeroless_y) == 1 and zeroless_x[0] >= min_filter and zeroless_y[0] >= min_filter:
+            print(zeroless_x[0], zeroless_y[0])
+            x = array_x.index(zeroless_x[0])
+            y = array_y.index(zeroless_y[0])
+            matrix[x, y] = matrix[x, y] + 1
+
---
+++ test.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scorpion_tgs_matrix as stm
+
+data = open('test.txt', 'r')
+
+count_matrix = np.array([[0.0] * 8] * 8)
+energy_matrix = np.array([[0.0] * 8] * 8)
+
+for i in data:
+    string = data.readline().split(', ')
+    string = [line.rstrip() for line in string]
+    string = list(map(float, string))
+    stm.create_energy_matrix(energy_matrix, string)
+    stm.create_events_count_matrix(count_matrix, string)
+
+print("ENERGY SUMMARY MATRIX:")
+print(energy_matrix)
+print("COUNT SUMMARY MATRIX:")
+print(count_matrix)
+
+fig, ax = plt.subplots()
+
+cax = ax.imshow(energy_matrix, cmap='YlOrBr', interpolation='none')
+
+fig.colorbar(cax)
+
+plt.title("Распределение энергии на матрице GAGG:Cr +Pol 1МэВ 0deg:")
+plt.show()
